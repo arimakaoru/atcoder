8075b7ea3ed1762bd89eece0fd2fb9379ec3b4e2
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2019-03-26 11:43:59 +0900

    abc122-cの答え合わせ

--- abc122/abc122-c.py
+++ abc122/abc122-c.py
@@ -1,10 +1,19 @@
-# TLE
+# TLE > AC
+# 累積和を使う
+# 出力は随時でもOK
+
 N, Q = map(int, input().split())
 S = input()
-ans = []
+
+t = [0] * N
+t[0] = 0
+for i in range(1,N):
+    if S[i-1] == 'A' and S[i] == 'C':
+        t[i] = t[i-1] + 1
+    else:
+        t[i] = t[i-1]
+
 for _ in range(Q):
     l, r = map(int, input().split())
-    ans.append(S[l-1:r].count('AC'))
+    print(t[r-1] - t[l-1])
 
-for i in ans:
-    print(i)
