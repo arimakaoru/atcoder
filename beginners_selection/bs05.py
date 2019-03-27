1b411019cc060bf8ce6c1a2cd2a64029aa190e55
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2019-03-27 10:25:40 +0900

    Add: bs05

---
+++ beginners_selection/bs05.py
@@ -0,0 +1,21 @@
+# pythonならO(10^7)くらいまでが2s制限の目安かも
+
+def main():
+    A = int(input())
+    B = int(input())
+    C = int(input())
+    X = int(input())
+
+    cnt = 0
+
+    for i in range(A+1):
+        for j in range(B+1):
+            for k in range(C+1):
+                if((500*i + 100*j + 50*k) == X):
+                    cnt += 1
+
+    print(cnt)
+
+
+if __name__ == '__main__':
+    main()
